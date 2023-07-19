e82c263c436f6e1fc6bc4a17fbf12108bac0b490
shubhamgitvns <93187281+shubhamgitvns@users.noreply.github.com>
2023-07-19 18:34:47 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# flutterfierebasedemo
+ 
---
+++ android/app/src/main/kotlin/com/example/flutterfierebasedemo/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutterfierebasedemo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
